8508005d4968f676af59eff4a66a0a6108d868dd
ALFAHMEE <fahmee.al@gmail.com>
2021-03-29 17:04:45 +0600

    uptaded

---
+++ dashboard.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    
+</head>
+<body>
+    <div class="form">
+        <p>Hey, <?php echo $_SESSION['username']; ?>!</p>
+        <p>You are now user dashboard page.</p>
+        <p><a href="logout.php">Logout</a></p>
+    </div>
+</body>
+</html>
